2d35d1c5fdec57b26561babede9ad7cc0eeb5bd9
russellballestrini <rballestrini@mobiquityinc.com>
2016-09-08 12:28:20 -0400

    gtfo
    	modified:   base/files/sethostname.sh
    	modified:   base/tasks/main.yml
    	deleted:    java/.travis.yml
    	deleted:    java/README.md
    	deleted:    java/defaults/main.yml
    	deleted:    java/meta/main.yml
    	new file:   java/tasks/main.yaml
    	deleted:    java/tasks/main.yml
    	deleted:    java/tasks/setup-Debian.yml
    	deleted:    java/tasks/setup-FreeBSD.yml
    	deleted:    java/tasks/setup-RedHat.yml
    	deleted:    java/tests/Dockerfile.centos-6
    	deleted:    java/tests/Dockerfile.centos-7
    	deleted:    java/tests/Dockerfile.ubuntu-12.04
    	deleted:    java/tests/Dockerfile.ubuntu-14.04
    	deleted:    java/tests/test.yml
    	deleted:    java/vars/Debian.yml
    	deleted:    java/vars/FreeBSD.yml
    	deleted:    java/vars/RedHat.yml
    	deleted:    java/vars/Ubuntu-12.04.yml
    	deleted:    java/vars/Ubuntu-14.04.yml
    	deleted:    java/vars/Ubuntu-16.04.yml

--- roles/base/files/sethostname.sh
+++ roles/base/files/sethostname.sh
@@ -7,8 +7,17 @@ newNodename="${newHostname%%.*}"
 [[ -z "$newHostname" ]] && exit 1
 [[ "$oldHostname" = "$newHostname" ]] && exit 0
 
+# set it in kernel memory.
 hostname "$newHostname"
-sed -i "s/HOSTNAME=.*/HOSTNAME=$newHostname/g" /etc/sysconfig/network
+
+# set it on filesystem.
+echo "$newHostname" > /etc/hostname
+
+# set it in this area just incase its being used to overide /etc/hostname/
+net_cfg="/etc/sysconfig/network"
+net_tmp="/etc/sysconfig/network.tmp"
+grep -v "HOSTNAME=" $net_cfg > $net_tmp && mv $net_tmp $net_cfg
+echo "HOSTNAME=$newHostname" >> $net_cfg
 
 # Clean up /etc/hosts
 hostIP="$(ip route get 8.8.8.8 | head -n1)"
@@ -21,5 +30,6 @@ sed -i "/$hostIP/d" /etc/hosts
 
 sed -i "1s/^/$hostIP $newHostname $newNodename\n/" /etc/hosts
 
+# reload network, maybe not needed, going to remove.
 service network reload
 exit 0
