1599902bf3b0a56cc92e0d2c89826dff5b34f57f
russellballestrini <rballestrini@mobiquityinc.com>
2017-02-22 15:22:11 -0500

    new file:   roles/alertlogic/files/send-to-alertlogic.conf 	new file:   roles/alertlogic/tasks/main.yml 	new file:   roles/automox/handlers/main.yml 	new file:   roles/automox/tasks/main.yml 	new file:   roles/automox/vars/main.yml 	new file:   roles/automox/vars/secret.yml 	new file:   roles/base/default/main.yml 	new file:   roles/base/tasks/hostname.yml 	modified:   roles/base/tasks/main.yml 	new file:   roles/certificates/default/main.yml 	copied:     roles/certificates/tasks/main.yml -> roles/certificates/tasks/create_certificates.yml 	modified:   roles/certificates/tasks/main.yml 	new file:   roles/jumpcloud/default/main.yml 	modified:   roles/jumpcloud/tasks/main.yml 	new file:   roles/jumpcloud/tasks/packer.yml 	new file:   roles/jumpcloud/tasks/register-and-tag.yml 	modified:   roles/mongodb/files/mongo-back.sh 	modified:   roles/mongodb/tasks/main.yml 	new file:   roles/mongodb/templates/mongo-back.conf.j2 	modified:   roles/mongodb/templates/mongod.conf.j2 	new file:   roles/rsyslog/default/main.yml 	new file:   roles/rsyslog/files/logrotate-syslog 	new file:   roles/rsyslog/files/rsyslog.repo 	new file:   roles/rsyslog/handlers/main.yml 	new file:   roles/rsyslog/tasks/main.yml 	new file:   roles/rsyslog/templates/send-to-remote.conf.j2

--- roles/mongodb/files/mongo-back.sh
+++ roles/mongodb/files/mongo-back.sh
@@ -1,30 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-mongodb_backup_dir="/tmp/mongodb-backups"
-mongodb_name_file="/tmp/mongodb-database-names"
+source /etc/default/mongo-back
+
+get_env_or_set_default=${mongodb_host:=localhost}
+get_env_or_set_default=${mongodb_backup_dir:=/tmp/mongodb-backups}
+get_env_or_set_default=${retention_days:=21}
+
 the_date=$(date +%Y-%m-%d-%H%M%S)
-retention_days=21
 
-mkdir -p $mongodb_backup_dir
+backup_dir="$mongodb_backup_dir/$the_date"
 
-echo 'db.adminCommand({ listDatabases: 1 })["databases"].forEach(function(x){print(x.name)});' | \
-  /usr/bin/mongo --quiet > $mongodb_name_file
+mkdir -p "$mongodb_backup_dir"
+mkdir -p "$backup_dir"
 
-echo ""
+/usr/bin/mongodump --quiet --host $mongodb_host -o $backup_dir
+tar -zcvf "$backup_dir.tar.gz" "$backup_dir"
+rm -rf "$backup_dir"
 
-while read dbname; do
-  echo "database $dbname:"
-  mongodb_collection_file="/tmp/mongodb-$dbname-collection-names"
-  echo 'show collections;' | /usr/bin/mongo $dbname --quiet > "$mongodb_collection_file"
-  while read cname; do
-    file_name="$dbname.$cname.$the_date.bson.gz"
-    gzip_file="$mongodb_backup_dir/$file_name"
-    echo "  dumping collection $cname into $gzip_file"
-    /usr/bin/mongodump --quiet -d $dbname -c $cname -o - | gzip -9 > "$gzip_file" 
-  done <$mongodb_collection_file
-  rm "$mongodb_collection_file"
-  echo ""
-done <$mongodb_name_file
-rm "$mongodb_name_file"
-
-find $mongodb_backup_dir -mindepth 1 -mtime +$retention_days -type f -delete -name *.gz
+find $mongodb_backup_dir -mindepth 1 -mtime +$retention_days -type f -delete -name "*.tar.gz"
